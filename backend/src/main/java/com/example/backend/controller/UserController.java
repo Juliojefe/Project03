d1343fab56bbbb986649c9f34920886e8da97c7e
Juliojefe <j.c.jf285@gmail.com>
2025-04-24 22:20:17 +0100

    manage user endpoints

--- backend/src/main/java/com/example/backend/controller/UserController.java
+++ backend/src/main/java/com/example/backend/controller/UserController.java
@@ -1,8 +1,6 @@
 package com.example.backend.controller;
 
-import com.example.backend.dto.UserLoginRequest;
-import com.example.backend.dto.UserLoginResponse;
-import com.example.backend.dto.UserRegisterRequest;
+import com.example.backend.dto.*;
 import com.example.backend.model.User;
 import com.example.backend.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -32,5 +30,44 @@ public class UserController {
         return userService.getAllUsers();
     }
 
+    @PatchMapping("/update-name/{id}/name")
+    public boolean updateUserName(@RequestBody UpdateNameRequest request) {
+        return userService.updateName(request);
+    }
+
+    @PatchMapping("update-email/{id}/email")
+    public boolean updateEmail(@RequestBody UpdateEmailRequest request) {
+        return userService.updateEmail(request);
+    }
+
+    @PatchMapping("update-password/{id}/password")
+    public boolean updatePassword(@RequestBody UpdatePasswordRequest request) {
+        return userService.updatePassword(request);
+    }
+
+    @PatchMapping("make-admin/{id}")
+    public boolean makeAdmin(@RequestBody MakeAdminRequest request) {
+        return userService.makeAdmin(request);
+    }
+
+    @PatchMapping("make-mechanic/{id}")
+    public boolean makeMechanic(@RequestBody MakeMechanicRequest request) {
+        return userService.makeMechanic(request);
+    }
+
+    @PatchMapping("make-regular-user/{id}")
+    public boolean makeRegularUser(@RequestBody MakeRegularUserRequest request) {
+        return userService.makeRegularUser(request);
+    }
+
+    @PatchMapping("update-profile-pic/{id}/url-new-pic")
+    public boolean updateProfilePic(@RequestBody UpdateProfilePicRequest request) {
+        return userService.updateProfilePic(request);
+    }
+
+    @DeleteMapping("/delete-user/{id}")
+    public boolean deleteUser(@RequestBody DeleteUserRequest request) {
+        return userService.deleteUser(request);
+    }
 
 }
